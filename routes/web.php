cb79387ef8e650729ea5baae89bfb9c2d54bde59
Unknown <russiancardcollector@mail.ru>
2017-11-27 15:08:50 +0300

    Добавлены маршруты

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'api/'], function() {
+    Route::resource('customer', 'CustomerController');
+    Route::resource('invoice', 'InvoiceController');
+});
